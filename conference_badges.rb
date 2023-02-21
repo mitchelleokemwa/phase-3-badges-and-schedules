aa2d79d27013ef88083f8c14dc261c093f576379
git_mitchelleokemwa <mitchelleokemwa@gmail.com>
2023-02-21 23:07:29 +0300

    Completed assignment

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def badge_maker(name="Ronald")
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    new_array=[]
+    array.each do |names|
+        new_array.push("Hello, my name is #{names}.")
+    end
+    return new_array
+end
+
+def assign_rooms array
+    room = []
+    array.each.with_index do |name,index| 
+        room.push("Hello, #{name}! You'll be assigned to room #{index+1}!") 
+    end
+    return room
+end
+
+def printer array
+    batch_badge_creator(array).each{|names| puts names}
+    assign_rooms(array).each{|names| puts names}
+end
